19219176e7096e6729acb5dcf1d9fdbc88a779d5
Yao Yuan <yaoyuan131617@gmail.com>
2016-03-25 17:20:48 +0800

    update html

--- static/app.js
+++ static/app.js
@@ -1,9 +1,15 @@
+import AppBar from 'material-ui/lib/app-bar';
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         Hello, Mathilda! I am a Watson.
       </div>
+      <AppBar
+        title="Title"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+      />
     );
   }
 });
