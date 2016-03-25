0c3eb575b18bbde5d4f7053d0bf916dd321d8845
Yao Yuan <yaoyuan131617@gmail.com>
2016-03-25 17:32:56 +0800

    update html

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-import AppBar from '../node_modules/material-ui/lib/app-bar';
+import AppBar from '../node_modules/material-ui/lib/app-bar.js';
 
 var CommentBox = React.createClass({
   render: function() {
