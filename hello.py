58feaf44467ddd09ae850fb9a6f4ceab065dbaf3
Yao Yuan <yaoyuan131617@gmail.com>
2016-03-25 12:02:51 +0800

    update icon

--- hello.py
+++ hello.py
@@ -7,4 +7,5 @@ def hello():
     return render_template('hello.html')
 
 if __name__ == "__main__":
+    app.add_url_rule('/favicon.ico',redirect_to=url_for('static', filename='favicon.ico'))
     app.run(host='0.0.0.0',port=80)
