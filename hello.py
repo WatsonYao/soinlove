b97920cc0d43bebebfe01ba567be97483d4ae473
Yao Yuan <yaoyuan131617@gmail.com>
2016-03-25 12:05:45 +0800

    update icon

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 from flask import render_template,Flask
+import os 
+from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('hello.html')
+    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
-    app.add_url_rule('/favicon.ico',redirect_to=url_for('static', filename='favicon.ico'))
     app.run(host='0.0.0.0',port=80)
